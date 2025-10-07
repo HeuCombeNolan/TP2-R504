9a510fc8593777b9750badcfa23a839ef2857e3b
HeuCombeNolan <153827820+HeuCombeNolan@users.noreply.github.com>
2025-10-07 14:53:48 +0200

    Update cryptage.py

--- cryptage.py
+++ cryptage.py
@@ -13,7 +13,7 @@ def crypt(message, pas=1):
     return resultat + str(pas)
 
 def decrypt(message):
-    # Le pas est toujours le dernier caractère
+    # Le pas est toujours le dernier caractere
     pas = int(message[-1])
     message = message[:-1]
     resultat = ""

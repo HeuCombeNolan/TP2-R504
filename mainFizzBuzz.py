043788469ae5d24d1cd55d55deb5ddcbe96ddb1c
HeuCombeNolan <153827820+HeuCombeNolan@users.noreply.github.com>
2025-10-07 14:55:27 +0200

    Update mainFizzBuzz.py

--- mainFizzBuzz.py
+++ mainFizzBuzz.py
@@ -3,7 +3,7 @@ from fizzbuzz import affiche
 # Test sans argument
 print(affiche())
 
-# Test avec un seul paramètre
+# Test avec un seul parametre
 print(affiche(15))
 
 # Test avec deux paramètres

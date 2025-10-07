26b360eeeec1a0d6660fa79c2b2778358d38dec2
HeuCombeNolan <153827820+HeuCombeNolan@users.noreply.github.com>
2025-10-07 14:01:22 +0200

    Create test_fizzbuzz.py
    Création du code.

---
+++ test_fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import unittest
+from fizzbuzz import affiche
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_affiche_base(self):
+        self.assertEqual(affiche(), ''.join(['Fizz' if i % 3 == 0 and i % 5 != 0
+                                             else 'Buzz' if i % 5 == 0 and i % 3 != 0
+                                             else 'FrisBee' if i % 15 == 0
+                                             else str(i)
+                                             for i in range(1, 101)]))
+
+if __name__ == '__main__':
+    unittest.main()

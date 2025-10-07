7199f1667415f893dcc3960e45c2d7e1239cbd82
HeuCombeNolan <153827820+HeuCombeNolan@users.noreply.github.com>
2025-10-07 15:00:05 +0200

    Update mainFizzBuzz.py

--- mainFizzBuzz.py
+++ mainFizzBuzz.py
@@ -1,14 +1,14 @@
 
 from fizzbuzz import affiche
 
-print("=== Test 1 : sans paramètre ===")
+print("=== Test 1 : sans parametre ===")
 print(affiche())
 print("-" * 50)
 
-print("=== Test 2 : avec un seul paramètre (15) ===")
+print("=== Test 2 : avec un seul parametre (15) ===")
 print(affiche(15))
 print("-" * 50)
 
-print("=== Test 3 : avec deux paramètres (5, 10) ===")
+print("=== Test 3 : avec deux parametres (5, 10) ===")
 print(affiche(5, 10))
 print("-" * 50)
